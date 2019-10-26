9fdff0a3ffbd72a782e99eb1b4cc53076a4184ba
IvanAndreiDaniel <56355504+IvanAndreiDaniel@users.noreply.github.com>
2019-10-26 19:45:15 +0200

    Prima versione (non funzionano.)
    Ho soltanto scritto il codice e guardato se compilano, ma provandoli non funzionano ovviamente; devo ancora sistemare tutto ciò che non funziona e guardare bene le slides di teoria.
    Se sistemate voi i file, scrivetelo su Telegram

---
+++ client_udp.c
@@ -0,0 +1,109 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <string.h>
+
+#define LINE_LENGTH 256
+
+// Struttura di una richiesta
+typedef struct {
+	char *file;
+} Request;
+
+int main(int argc, char *argv[]) {
+	struct hostent *host;
+	struct sockaddr_in clientaddr, servaddr;
+	int portNumber, datagramSocket, number, length, result;
+	char *fileName;
+	Request request;
+	
+	// Controllo argomenti in input
+	if (argc != 3) {
+		printf("Errore. Utilizzo del programma: %s serverAddress serverPort\n", argv[0]);
+		exit(1);
+	}
+
+	// Inizializzazione indirizzo Client e Server
+	memset((char *)&clientaddr, 0, sizeof(struct sockaddr_in));
+	clientaddr.sin_family = AF_INET;
+	clientaddr.sin_addr.s_addr = INADDR_ANY;
+
+	// Passando 0 ci leghiamo ad un qualsiasi indirizzo libero
+	clientaddr.sin_port = 0;
+
+	memset((char *)&servaddr, 0, sizeof(struct sockaddr_in));
+	servaddr.sin_family = AF_INET;
+	host = gethostbyname(argv[1]);
+
+	// Controllo che la porta passata come argomento sia formata da soli numeri
+	number = 0;
+	while (argv[2][number] != '\0') {
+		if ((argv[2][number] < '0') || (argv[2][number] > '9')) {
+			printf("Secondo argomento non intero.\n");
+			printf("Errore. Utilizzo del programma: %s serverAddress serverPort\n", argv[0]);
+			exit(2);
+		}
+		number++;
+	}
+
+	portNumber = atoi(argv[2]);
+
+	// Verifica porta e host
+	if (portNumber < 1024 || portNumber > 65535) {
+		printf("La porta passata in input deve essere compresa tra 1024 e 65535.\n");
+		exit(3);
+	}
+
+	if (host == NULL) {
+		printf("L'host non è stato trovato.\n");
+		exit(4);
+	} else {
+		servaddr.sin_addr.s_addr = ((struct in_addr *)(host->h_addr))->s_addr;
+		servaddr.sin_port = htons(portNumber);
+	}
+
+	// Creazione socket
+	datagramSocket = socket(AF_INET, SOCK_DGRAM, 0);
+	if (datagramSocket < 0) {
+		perror("Apertura socket.");
+		exit(5);
+	}
+	printf("Creata la socket %d.\n", datagramSocket);
+
+	// Bind socket a una porta scelta dal sistema
+	if (bind(datagramSocket, (struct sockaddr*)&clientaddr, sizeof(clientaddr)) <0) {
+		perror("Bind socket.");
+		exit(6);
+	}
+	printf("Bind socket riuscito alla porta %i\n", clientaddr.sin_port);
+
+	// Inizio del programma effettivo
+
+	printf("Inserire il nome di un file o EOF per terminare: ");
+
+	while (gets(&fileName) != EOF) {
+		request.file = htonl(fileName);
+		
+		length = sizeof(servaddr);
+		if (sendto(datagramSocket, &request, sizeof(Request), 0, (struct sockaddr*)&servaddr, length) < 0) {
+			perror("Errore nella sendto.");
+			continue;
+		}
+
+		if (recvfrom(datagramSocket, &result, sizeof(result), 0, (struct sockaddr*)&servaddr, &length) < 0) {
+			perror("Errore nella recvfrom.");
+			continue;
+		}
+	
+		printf("La parola piu' lunga nel file richiesto ha %i caratteri.\n", (int)ntohl(result));
+		printf("Inserire il nome di un file o EOF per terminare: ");
+	}
+
+	close(datagramSocket);
+	printf("Il Client sta terminando...\n");  
+	exit(0);
+}
---
+++ server_udp.c
@@ -0,0 +1,119 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <string.h>
+
+#define MAX_STRING_LENGTH 255
+
+// Struttura di una richiesta
+typedef struct {
+	char *file;
+} Request;
+
+int main(int argc, char *argv[]) {
+	int datagramSocket, portNumber, length, number, fd, longestWord;
+	const int on = 1;
+	struct sockaddr_in cliaddr, servaddr;
+	struct hostent *clienthost;
+	Request *request = (Request*) malloc(sizeof(Request));
+	char *temp, line[MAX_STRING_LENGTH];
+
+	// Controllo argomenti
+	if (argc != 2) {
+		printf("Errore. Utilizzo del programma: %s serverPort\n", argv[0]);
+		exit(1);
+	}
+
+	number = 0;
+	while (argv[1][number] != '\0') {
+		if ((argv[1][number] < '0') || (argv[1][number] > '9')) {
+			printf("Secondo argomento non intero.\n");
+			printf("Errore. Utilizzo del programma: %s serverPort\n", argv[0]);
+			exit(2);
+		}
+		number++;
+	}
+
+	portNumber = atoi(argv[2]);
+
+	// Verifica porta
+	if (portNumber < 1024 || portNumber > 65535) {
+		printf("La porta passata in input deve essere compresa tra 1024 e 65535.\n");
+		exit(3);
+	}
+
+	// Inizializzazione indirizzo Server
+	memset((char *)&servaddr, 0, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr = INADDR_ANY;
+	servaddr.sin_port = htons(portNumber);
+
+	// Creazione, settaggio opzioni e connessione socket
+	datagramSocket = socket(AF_INET, SOCK_DGRAM, 0);
+	if (datagramSocket < 0) {
+		perror("Creazione socket.");
+		exit(4);
+	}
+	printf("Creata la socket %d.\n", datagramSocket);
+
+	if (setsockopt(datagramSocket, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on)) < 0) {
+		perror("Set opzioni socket.");
+		exit(5);
+	}
+	printf("Set opzioni socket ok.\n");
+
+	if (bind(datagramSocket, (struct sockaddr*)&servaddr, sizeof(servaddr)) < 0) {
+		perror("Bind socket.");
+		exit(6);
+	}
+	printf("Bind socket ok.\n");
+
+	// Ciclo di ricezione richieste
+	for(;;) {
+		length = sizeof(struct sockaddr_in);
+		if (recvfrom(datagramSocket, request, sizeof(Request), 0, (struct sockaddr*)&cliaddr, &length) < 0) {
+			perror("Errore nella recvfrom.");
+			continue;
+		}
+
+		clienthost = gethostbyaddr((char *)&cliaddr.sin_addr, sizeof(cliaddr.sin_addr), AF_INET);
+		if (clienthost == NULL) {
+			printf("Non sono state trovate informazioni sull'host.\n");
+		}
+		else {
+			printf("Operazione richiesta da: %s %i\n", clienthost->h_name, (unsigned)ntohs(cliaddr.sin_port));
+		}
+
+		longestWord = 0;
+		fd = open(request->file, O_RDONLY, 00640);
+
+		if (fd < 0) {
+			longestWord = -1;
+		} else {
+			while (fgets(line, MAX_STRING_LENGTH, fd)) {
+				temp = strtok(line, " ");
+				while (temp != NULL) {
+					if (strlen(temp) > longestWord) {
+						longestWord = strlen(temp);
+					}
+				}
+				temp = strtok(NULL, " ");
+			}
+
+			close(fd);
+		}
+
+		longestWord = htonl(longestWord);
+		if (sendto(datagramSocket, &longestWord, sizeof(longestWord), 0, (struct sockaddr*)&cliaddr, length) < 0) {
+			perror("Errore nella sendto."); 
+			continue;
+		}
+	}
+}
